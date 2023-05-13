ea30155640b75f148bddd9e781c593b06f105ab1
tqlong1609 <tqlong1609@gmail.com>
2023-05-14 00:36:01 +0700

    [Add] copy redux folder in remote

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,51 @@
 #! /usr/bin/env node
-var fs = require('fs');
-const path = require('path');
-// const yargs = require('yargs');
-// console.log(yargs.argv.value);
+// var fs = require('fs');
+// const path = require('path');
+// // const yargs = require('yargs');
+// // console.log(yargs.argv.value);
 
-fs.cp(path.resolve('./') + '/src/redux', './redux', { recursive: true }, (err) => {
-  if (err) {
-    console.error(err);
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+// GitHub repository URL
+// const repoUrl = 'https://github.com/tqlong1609/front-end-common/tree/master/src/redux';
+const repoUrl = 'https://api.github.com/repos/tqlong1609/front-end-common/contents/src/redux';
+
+// Local directory where you want to save the downloaded folder
+const localDir = './redux';
+
+// Function to download a file
+const downloadFile = async (url, filePath) => {
+  const response = await axios.get(url, { responseType: 'arraybuffer' });
+  fs.writeFileSync(filePath, response.data);
+};
+
+// Function to download a folder
+const downloadFolder = async (folderUrl, localFolderPath) => {
+  const response = await axios.get(folderUrl);
+  const files = response.data;
+
+  if (!fs.existsSync(localFolderPath)) {
+    fs.mkdirSync(localFolderPath);
   }
-});
+
+  for (const file of files) {
+    const filePath = path.join(localFolderPath, file.name);
+
+    if (file.type === 'dir') {
+      await downloadFolder(file.url, filePath);
+    } else {
+      await downloadFile(file.download_url, filePath);
+    }
+  }
+};
+
+// Download the folder from the repository
+downloadFolder(repoUrl, localDir)
+  .then(() => {
+    console.log('Folder downloaded successfully.');
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
